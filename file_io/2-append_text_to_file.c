923d2f64b1c2f13ef870c729ff34f3db2ea43130
hug0-c <hugo.casteras31@gmail.com>
2023-04-10 08:56:08 -0700

    add comments

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,18 +10,33 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, writing;
 
 	if (filename == NULL)
+	{
 		return (-1);
+	}
+
 	if (text_content == NULL)
+	{
 		return (1);
+	}
 
+	/* Open the file in append mode and write-only mode */
 	fd = open(filename, O_APPEND | O_WRONLY);
+	
+	/* Check if the file could be opened */
 	if (fd == -1)
+	{
 		return (-1);
-
+	}
+	
+	/* Write the text_content to the file */
 	writing = write(fd, text_content, strlen(text_content));
+	/* Check if the write operation was successful */
 	if (writing == -1)
+	{
 		return (-1);
+	}
 
 	close(fd);
+
 	return (1);
 }
