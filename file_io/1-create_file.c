08871c24baa6ba220c6f841d04f9b51e6ba20d52
hug0-c <hugo.casteras31@gmail.com>
2023-04-10 09:08:13 -0700

    add comments

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,22 +14,32 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
+	/* Open the file with O_CREAT flag to create a new file if it doesn't exist and O_WRONLY to allow write-only access to the file */
+	/* Use O_TRUNC to truncate the file to length 0 if it already exists */
+	/* Use 0600 as the mode to set the file permissions to rw------- (read and write only for owner) */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	/* Check if the file could be opened */
 	if (fd == -1)
 	{
+		/* If the file could not be opened, return -1 to indicate an error */
 		return (-1);
 	}
 
-	if (!text_content)
+	/* Check if text_content is NULL */
+	if (text_content == NULL)
 	{
+		/* If text_content is NULL, set it to an empty string */
 		text_content = "";
 	}
 
+	/* Write the text_content to the file */
 	writing = write(fd, text_content, strlen(text_content));
 
+	/* Check if the write operation was successful */
 	if (writing == -1)
 	{
+		/* If the write operation failed, return -1 to indicate an error */
 		return (-1);
 	}
 
