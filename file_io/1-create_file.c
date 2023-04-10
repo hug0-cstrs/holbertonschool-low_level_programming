c66dc6f91f670b8835ad0f1f7bf0c5ee53c6211d
hug0-c <hugo.casteras31@gmail.com>
2023-04-10 08:22:33 -0700

    Create a function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,14 +14,14 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	fd = creat(filename, O_RDONLY | O_WRONLY | O_TRUNC | 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
 		return (-1);
 	}
 
-	if (text_content == NULL)
+	if (!text_content)
 	{
 		text_content = "";
 	}
@@ -34,5 +34,6 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
+
 	return (1);
 }
