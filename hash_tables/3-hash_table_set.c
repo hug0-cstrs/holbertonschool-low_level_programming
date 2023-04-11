15ae1386cc0c4b5bc3fcab722a82153c316b025b
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-11 16:47:16 +0200

    Write a function that adds an element to the hash table

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: hash table
+ * @key: current key
+ * @value: value associated with the key
+ * Return: 1 if succeed or 0 if not
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index;
+    hash_node_t *temp, *new;
+    /* Vérifie que les paramètres ne sont pas nuls */
+    if (ht == NULL || key == NULL || value == NULL)
+    {
+        return (0);
+    }
+    /* Calcule l'index correspondant à la clé */
+    index = key_index((const unsigned char *)key, ht->size);
+    /* Initialise un pointeur à la position correspondante dans la table de hachage */
+    temp = ht->array[index];
+
+    while (temp != NULL)
+    {
+        /* Si la clé est trouvée, il met à jour la valeur associée */
+            if (strcmp(temp->key, key) == 0)
+            {
+                temp->value = strdup(value);
+                return (1);
+            }
+            temp = temp->next;
+    }
+
+    /* Si la clé n'existe pas encore, crée une nouvelle clé avec sa valeur*/
+    new = malloc(sizeof(hash_node_t));
+    if (new == NULL)
+    {
+        return (0);
+    }
+    new->key = strdup(key);
+    new->value = strdup(value);
+    new->next = NULL;
+
+    return (1);
+}
