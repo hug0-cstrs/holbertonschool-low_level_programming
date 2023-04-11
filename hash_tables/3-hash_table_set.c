d9de10916fb1db7d5f48cb2ba0013670e29b98a9
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-11 17:39:12 +0200

    correct betty errors

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,20 +10,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *temp, *new;
-	/* Vérifie que les paramètres ne sont pas nuls */
+
 	if (ht == NULL || key == NULL || value == NULL)
 	{
 		return (0);
 	}
-	/* Calcule l'index correspondant à la clé */
+
 	index = key_index((const unsigned char *)key, ht->size);
-	/* Initialise un pointeur à la position correspondante dans la table de hachage */
+
 	temp = ht->array[index];
 
-    /* Parcours les nodes suivants jusqu'à trouver la clé ou la fin de la liste */
-    while (temp != NULL)
+
+	while (temp != NULL)
 	{
-		/* Si la clé est trouvée, il met à jour la valeur associée */
+
 		if (strcmp(temp->key, key) == 0)
 		{
 			temp->value = strdup(value);
@@ -32,7 +32,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = temp->next;
 	}
 
-	/* Si la clé n'existe pas encore, crée une nouvelle clé avec sa valeur*/
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 	{
@@ -42,13 +41,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->value = strdup(value);
 	new->next = NULL;
 
-	/* Ajoute la nouvelle node en tête de liste, si la position était déjà occupée */
 	if (ht->array[index] != NULL)
 	{
 		new->next = ht->array[index];
 	}
 
-	/* Met à jour le pointeur ht pour pointer sur la nouvelle paire */
 	ht->array[index] = new;
 
 	return (1);
