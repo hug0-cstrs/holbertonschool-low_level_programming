66d2c26955a971a5233fe384bed90cd9150155ce
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-11 19:33:36 +0200

    add comments

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -4,15 +4,22 @@
  *
  * Return: hash value
  */
+
+/* This function generates a hash value using the djb2 algorithm for a given string input */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
+	/* Initialize hash to a number */
+	unsigned long int hash = 5381;
+	/* Declare a variable to hold the current character being processed */
 	int c;
 
-	hash = 5381;
+	/* Loop through the string until the null terminator is reached */
 	while ((c = *str++))
 	{
+		/* Generate the hash value using the djb2 algorithm */
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
+
+	/* Return the hash value */
 	return (hash);
 }
