a397a74d8f96f24553736649fe7b1f2a6527934f
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-11 19:25:50 +0200

    add comments

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,21 +8,32 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	/* Initialize a pointer to NULL */
 	hash_table_t *hashtable = NULL;
 
+	/* Allocate memory for hash_table_t struct and assign it to the pointer */
 	hashtable = malloc(sizeof(hash_table_t));
+
+	/* Check if the allocation was successful */
 	if (hashtable == NULL)
 	{
 		return (NULL);
 	}
 
+	/* Set the size of the hash table */
 	hashtable->size = size;
+
+	/* Allocate memory for the array of pointers to hash_node_t structs */
 	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+
+	/* Check if the allocation was successful */
 	if (hashtable->array == NULL)
 	{
+		/* Free the previously allocated memory and return NULL if the allocation failed */
 		free(hashtable);
 		return (NULL);
 	}
 
+	/* Return a pointer to the created hash table */
 	return (hashtable);
 }
