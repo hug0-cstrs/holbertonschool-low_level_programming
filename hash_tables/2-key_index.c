0edbdfb946d0d6a9c718919ca3a27c3f3dd9919c
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-11 19:46:10 +0200

    add comments

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -6,12 +6,18 @@
  * Return: value of the index of the key related
  */
 
+/* This function takes in a key (a string input as an unsigned char pointer) and a size (the size of the hash table) and returns an index in the hash table where the key-value pair should be stored*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	/* Initialize a variable to hold the hash value generated from the key */
 	unsigned long int value = 0;
 
+	/* Generate a hash value using the djb2 algorithm for the key */
 	value = hash_djb2(key);
+
+	/* Take the modulo of the hash value with the size of the hash table */
 	value = value % size;
 
+	/* Return the resulting index in the hash table */
 	return (value);
 }
