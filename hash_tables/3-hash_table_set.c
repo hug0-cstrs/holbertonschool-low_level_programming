6b220c0b8de148eba02f6cd4dfe05feeaeb56049
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-11 19:41:48 +0200

    add comments

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -8,48 +8,48 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *temp, *new;
-	/* Vérifie que les paramètres ne sont pas nuls */
-	if (ht == NULL || key == NULL || value == NULL)
-	{
-		return (0);
-	}
-	/* Calcule l'index correspondant à la clé */
-	index = key_index((const unsigned char *)key, ht->size);
-	/* Initialise un pointeur à la position correspondante dans la table de hachage */
-	temp = ht->array[index];
+    unsigned long int index;
+    hash_node_t *temp, *new;
+    /* Vérifie que les paramètres ne sont pas nuls */
+    if (ht == NULL || key == NULL || value == NULL)
+    {
+        return (0);
+    }
+    /* Calcule l'index correspondant à la clé */
+    index = key_index((const unsigned char *)key, ht->size);
+    /* Initialise un pointeur à la position correspondante dans la table de hachage */
+    temp = ht->array[index];
 
     /* Parcours les nodes suivants jusqu'à trouver la clé ou la fin de la liste */
     while (temp != NULL)
-	{
-		/* Si la clé est trouvée, il met à jour la valeur associée */
-		if (strcmp(temp->key, key) == 0)
-		{
-			temp->value = strdup(value);
-			return (1);
-		}
-		temp = temp->next;
-	}
+    {
+        /* Si la clé est trouvée, il met à jour la valeur associée */
+        if (strcmp(temp->key, key) == 0)
+        {
+            temp->value = strdup(value);
+            return (1);
+        }
+        temp = temp->next;
+    }
 
-	/* Si la clé n'existe pas encore, crée une nouvelle clé avec sa valeur*/
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		return (0);
-	}
-	new->key = strdup(key);
-	new->value = strdup(value);
-	new->next = NULL;
+    /* Si la clé n'existe pas encore, crée une nouvelle clé avec sa valeur*/
+    new = malloc(sizeof(hash_node_t));
+    if (new == NULL)
+    {
+        return (0);
+    }
+    new->key = strdup(key);
+    new->value = strdup(value);
+    new->next = NULL;
 
-	/* Ajoute la nouvelle node en tête de liste, si la position était déjà occupée */
-	if (ht->array[index] != NULL)
-	{
-		new->next = ht->array[index];
-	}
+    /* Ajoute la nouvelle node en tête de liste, si la position était déjà occupée */
+    if (ht->array[index] != NULL)
+    {
+        new->next = ht->array[index];
+    }
 
-	/* Met à jour le pointeur ht pour pointer sur la nouvelle paire */
-	ht->array[index] = new;
+    /* Met à jour le pointeur ht pour pointer sur la nouvelle paire */
+    ht->array[index] = new;
 
-	return (1);
+    return (1);
 }
