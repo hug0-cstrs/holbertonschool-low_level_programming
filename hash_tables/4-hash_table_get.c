4b38b2b73adb649b56f94e37b87c0557c1cd568d
hug0-cstrs <hugo.casteras@outlook.fr>
2023-04-12 11:11:24 +0200

    add comments

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,26 +10,27 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *temp;
+	unsigned long int index; /* Déclaration d'une variable pour stocker l'index */
+	hash_node_t *temp;       /* Déclaration d'un pointeur pour stocker un noeud temporaire */
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL) /* Si la table de hachage ou la clé est NULL, renvoyer NULL */
 	{
 		return (NULL);
 	}
 
-	index = key_index((const unsigned char *)key, ht->size);
-	temp = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size); /* Obtenir l'index de la clé en utilisant la fonction key_index()*/
 
-	if (temp == NULL)
+	temp = ht->array[index]; /* Récupérer le noeud à l'index correspondant */
+
+	if (temp == NULL) /* Si le noeud est NULL, cela signifie qu'il n'y a pas d'élément correspondant à la clé */
 	{
 		return (NULL);
 	}
 
-	while (strcmp(temp->key, key) != 0)
+	while (strcmp(temp->key, key) != 0) /* Tant que la clé du noeud temporaire ne correspond pas à la clé recherchée */
 	{
-		temp = temp->next;
+		temp = temp->next; /* Passer au noeud suivant */
 	}
 
-	return (temp->value);
+	return (temp->value); /* Renvoyer la valeur associée à la clé trouvée */
 }
